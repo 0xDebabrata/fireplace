da80e3f4a47144d4272c125cf1f0a6be65b7f2ec
Debabrata Mondal <debabrata.js@protonmail.com>
2021-12-29 21:33:53 +0530

    [chat] add draggable sidebar

---
+++ functions/sidebar.js
@@ -0,0 +1,28 @@
+let drag = false 
+
+const handleMouseMove = (e) => {
+    const border = document.getElementById("border")
+    const percent = 100 - (e.clientX / window.innerWidth)*100
+    border.style.width = percent + "%"
+}
+
+export const handleClick = (e) => {
+    e.preventDefault()
+    const border = document.getElementById("border")
+    const rect = border.getBoundingClientRect()
+    const clickPointX = e.clientX
+    const left = rect.left
+    const padding = 2 
+
+    if (clickPointX <= left + padding) {
+        drag = true
+        document.addEventListener("mousemove", handleMouseMove)
+    }
+}
+
+export const handleMouseUp = () => {
+    if (drag) {
+        document.removeEventListener("mousemove", handleMouseMove) 
+        drag = false
+    }
+}
