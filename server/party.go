21bcdec3784579eef2f0d143629520f30c8e126e
dinosoupy <anishbasoup@gmail.com>
2023-11-16 20:09:45 +1100

    test

--- server/party.go
+++ server/party.go
@@ -8,110 +8,112 @@ import (
 )
 
 type PartyInfo struct {
-    Id          string      `json:"id"`
-    OwnerId     string      `json:"ownerId"`
-    Src         string      `json:"src"`
-    Playhead    float32     `json:"playhead"`
-    IsPlaying   bool        `json:"isPlaying"`
+	Id        string  `json:"id"`
+	OwnerId   string  `json:"ownerId"`
+	Src       string  `json:"src"`
+	Playhead  float32 `json:"playhead"`
+	IsPlaying bool    `json:"isPlaying"`
 }
 
 type Party struct {
-    party PartyInfo
-    //clients map[*Client]bool
-    clients sync.Map
-    broadcast chan []byte
-    join chan *Client
-    leave chan *Client
+	party PartyInfo
+	//clients map[*Client]bool
+	clients   sync.Map
+	broadcast chan []byte
+	join      chan *Client
+	leave     chan *Client
 }
 
 func createParty(id string, ownerId string, src string) *Party {
-    return &Party{
-        party: PartyInfo{
-            Id: id,
-            OwnerId: ownerId,
-            Src: src,
-            Playhead: 0,
-            IsPlaying: false,
-        },
-        //clients: make(map[*Client]bool),
-        clients: sync.Map{},
-        broadcast: make(chan []byte),
-        join: make(chan *Client),
-        leave: make(chan *Client),
-    }
+	return &Party{
+		party: PartyInfo{
+			Id:        id,
+			OwnerId:   ownerId,
+			Src:       src,
+			Playhead:  0,
+			IsPlaying: false,
+		},
+		//clients: make(map[*Client]bool),
+		clients:   sync.Map{},
+		broadcast: make(chan []byte),
+		join:      make(chan *Client),
+		leave:     make(chan *Client),
+	}
 }
 
 func (p *Party) run() {
-    log.Println("watchparty " + p.party.Id + " live.")
-    
-    done = make(chan bool)
-    go p.broadcastStatus(done)
-    
-    // watchparty will be terminated after this ticker ticks
-    ticker := time.NewTicker(24 * time.Hour)
-    defer func() {
-        done <-true // close broadcastStatus goroutine
-        ticker.Stop() // close watchparty
-    }()
+	log.Println("watchparty " + p.party.Id + " live.")
 
-    for {
-        select {
-        case client := <-p.join:
-            log.Printf("%s joined.\n", client.nickname)
-            //p.clients[client] = true
-            p.clients.Store(client, true)
-            handleJoin(p.party, client.send)    // Send party details to newly joined client
-            broadcastJoinOrLeave("new", &p.clients, client)  // Send join notification to peers
+	done := make(chan bool)
+	go p.broadcastStatus(done)
 
-        case client := <-p.leave:
-            //_, ok := p.clients[client]
-            _, ok := p.clients.Load(client)
-            if ok {
-                //delete(p.clients, client)
-                p.clients.Delete(client)
-                close(client.send)
-                broadcastJoinOrLeave("leave", &p.clients, client)  // Send join notification to peers
-            }
+	// watchparty will be terminated after this ticker ticks
+	ticker := time.NewTicker(24 * time.Second)
+	defer func() {
+		done <- true  // close broadcastStatus goroutine
+		ticker.Stop() // close watchparty
+	}()
 
-        case message := <-p.broadcast:
-            msg := wsMessage{}
-            json.Unmarshal([]byte(message), &msg)
-            p.clients.Range(func(key, value any) bool {
-                // Do not broadcast message back to the sender
-                if msg.ClientId != key.(*Client).id {
-                    select {
-                    case key.(*Client).send <- message:
-                    default:
-                        close(key.(*Client).send)
-                        //delete(p.clients, client)
-                        p.clients.Delete(key)
-                    }
-                }
-                return true
-            })
-        }
+	for {
+		select {
+		case client := <-p.join:
+			log.Printf("%s joined.\n", client.nickname)
+			//p.clients[client] = true
+			p.clients.Store(client, true)
+			handleJoin(p.party, client.send)                // Send party details to newly joined client
+			broadcastJoinOrLeave("new", &p.clients, client) // Send join notification to peers
 
-        case t := <-ticker.C:
-            // close the party after 24 hours
-            log.Prinf("Party %s terminated", p.party.Id)
-            return
-    }
+		case client := <-p.leave:
+			//_, ok := p.clients[client]
+			_, ok := p.clients.Load(client)
+			if ok {
+				//delete(p.clients, client)
+				p.clients.Delete(client)
+				close(client.send)
+				broadcastJoinOrLeave("leave", &p.clients, client) // Send join notification to peers
+			}
+
+		case message := <-p.broadcast:
+			msg := wsMessage{}
+			json.Unmarshal([]byte(message), &msg)
+			p.clients.Range(func(key, value any) bool {
+				// Do not broadcast message back to the sender
+				if msg.ClientId != key.(*Client).id {
+					select {
+					case key.(*Client).send <- message:
+					default:
+						close(key.(*Client).send)
+						//delete(p.clients, client)
+						p.clients.Delete(key)
+					}
+				}
+				return true
+			})
+
+		case <-ticker.C:
+			// close the party after 24 hours
+			log.Printf("Party %s terminated", p.party.Id)
+			return
+		}
+
+	}
 }
 
 func (p *Party) broadcastStatus(done chan bool) {
-    ticker := time.NewTicker(2000 * time.Millisecond)
+	ticker := time.NewTicker(2000 * time.Millisecond)
 
-    defer func() {
-        ticker.Stop()
-        log.Printf("%s party ticker stopped\n", p.party.Id)
-    }()
+	defer func() {
+		ticker.Stop()
+		log.Printf("%s party ticker stopped\n", p.party.Id)
+	}()
 
-    for {
-        select {
-        case <- done:
-            return
-        case <-ticker.C:
-            broadcastWatchpartyStatus(p)
-        }
-    }
+	for {
+		select {
+		case <-done:
+
+			return
+		case <-ticker.C:
+			broadcastWatchpartyStatus(p)
+		}
+	}
 }
--- server/party_test.go
+++ server/party_test.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestRun() {
+func TestRun(t *testing.T) {
 	party := createParty("party-123", "user-123", "https://youtube.com")
 
-    go party.run()
+	party.run()
 	// call create party
 	// party.run
 	// wait for end message
