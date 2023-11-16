fb4d95a99f491bec4b014438d975a70cc4c7ce0f
Debabrata Mondal <debabrata.js@protonmail.com>
2023-11-16 15:46:17 +0530

    client test

--- server/client_test.go
+++ server/client_test.go
@@ -2,11 +2,94 @@ package main
 
 import (
 	// "fmt"
+	"os"
+	"os/signal"
 	"testing"
 	"time"
+	"http"
+	"github.com/gorilla/websocket"
 )
 
 func TestClient(t *testing.T) {
 	go main()
-	time.Sleep(10*time.Second)
+	time.Sleep(1 * time.Second)
+	createClient()
+	time.Sleep(12*time.Second)
+}
+
+func createClient() {
+	url := "http://localhost:6969/create?userId=user-123&partyId=party-123&src=https://example.com"
+
+	// create watchparty
+	resp, err := http.Get(url)
+	log.Println(string(resp))
+	if err != nil {
+		log.Println("error making request", err)
+	}
+
+	// join as client
+	wsClient()
+}
+
+func wsClient() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{
+		Scheme: "ws",
+		Host: "localhost:6969",
+		Path: "/ws?userId=user-234&partyId=party-123"
+	}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
