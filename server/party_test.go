c4c9f7405000b14c35bd0ff8d333a554813cdb6d
Debabrata Mondal <debabrata.js@protonmail.com>
2023-11-16 15:10:35 +0530

    test

--- server/party_test.go
+++ server/party_test.go
@@ -7,7 +7,7 @@ import (
 func TestRun(t *testing.T) {
 	party := createParty("party-123", "user-123", "https://youtube.com")
 
-	party.run()
+	go party.run()
 	// call create party
 	// party.run
 	// wait for end message
