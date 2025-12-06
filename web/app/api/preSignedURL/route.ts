b3549d23b6d67f4b57c7298c6a962a9196901232
Debabrata Mondal <debabrata.js@protonmail.com>
2025-12-06 15:43:25 +0530

    update bucket config

--- web/app/api/preSignedURL/route.ts
+++ web/app/api/preSignedURL/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand, S3ClientConfig } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const S3_BUCKET = "fireplace-videos";
-const REGION = "us-east-1";
+const S3_BUCKET = "fireplace-videos-6e0c";
+const REGION = "us-west-1";
 const URL_EXPIRATION_TIME = 18000; // in seconds (5 hrs)
 
 const s3Config: S3ClientConfig = {
